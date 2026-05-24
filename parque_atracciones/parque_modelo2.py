5f28f23aab8f015c2c16903ef8955cda0676cd40
Asun M. Mayoral <asun.mayoral@umh.es>
2026-05-24 19:21:56 +0200

    Add functionality to download and import parque_comunes
    Download parque_comunes.py from a remote URL and import it.

--- parque_atracciones/parque_modelo2.py
+++ parque_atracciones/parque_modelo2.py
@@ -3,6 +3,11 @@
 # Versión adaptada a parámetros y funciones genéricas
 # ============================================================
 
+
+from urllib.request import urlretrieve
+url = "https://raw.githubusercontent.com/UMH1477/python/refs/heads/main/parque_atracciones/parque_comunes.py"
+urlretrieve(url, 'parque_comunes.py')
+from parque_comunes import *  
 import simpy
 import numpy as np
 import pandas as pd
