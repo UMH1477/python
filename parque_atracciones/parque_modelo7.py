6da457a85d03fbe6fffaf9f850c81465603b284a
Asun M. Mayoral <asun.mayoral@umh.es>
2026-05-24 19:23:18 +0200

    Update parque_modelo7.py

--- parque_atracciones/parque_modelo7.py
+++ parque_atracciones/parque_modelo7.py
@@ -3,6 +3,10 @@
 #           Restauración (M/M/c), Descanso (capacidad finita),
 #           Espectáculo (funciones programadas)
 # ============================================================
+from urllib.request import urlretrieve
+url = "https://raw.githubusercontent.com/UMH1477/python/refs/heads/main/parque_atracciones/parque_comunes.py"
+urlretrieve(url, 'parque_comunes.py')
+from parque_comunes import *  
 
 import simpy
 import numpy as np
