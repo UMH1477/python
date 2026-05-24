537ebc0be2069f15268f9d423f8fb59057eaa7b4
Asun M. Mayoral <asun.mayoral@umh.es>
2026-05-24 19:23:05 +0200

    Add download and import for parque_comunes.py
    Download parque_comunes.py from a remote repository and import its contents.

--- parque_atracciones/parque_modelo6.py
+++ parque_atracciones/parque_modelo6.py
@@ -2,6 +2,10 @@
 # MODELO 6. MODELO 5 + PERFILES + TOLERANCIA A LA ESPERA
 #           + MÉTRICAS POR FRANJA, PERFIL Y PERFIL/FRANJA
 # ============================================================
+from urllib.request import urlretrieve
+url = "https://raw.githubusercontent.com/UMH1477/python/refs/heads/main/parque_atracciones/parque_comunes.py"
+urlretrieve(url, 'parque_comunes.py')
+from parque_comunes import *  
 
 import simpy
 import numpy as np
